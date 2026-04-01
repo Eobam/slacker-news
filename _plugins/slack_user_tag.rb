a49ae16271ca6177e796d37f3a1ed98f8891a52d
24c02 <163450896+24c02@users.noreply.github.com>
2026-04-01 19:47:07 -0400

    plug the in!

---
+++ _plugins/slack_user_tag.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# use like so: {% slack_user UDK5M9Y13, cwalker %} => @cwalker, but nicely linked
+module SlackUserTag
+  SLACK_BASE_URL = "https://hackclub.slack.com/team"
+
+  class Tag < Liquid::Tag
+    def initialize(tag_name, markup, tokens)
+      super
+      parts = markup.split(",").map(&:strip)
+      @id   = parts[0]
+      @name = parts[1]
+    end
+
+    def render(_ctx) = %[<a href="#{SLACK_BASE_URL}/#{@id}" class="slack_user" target="_blank">@#{@name}</a>]
+  end
+end
+
+Liquid::Template.register_tag("slack_user", SlackUserTag::Tag)
