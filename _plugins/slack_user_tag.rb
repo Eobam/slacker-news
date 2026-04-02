476d59d5ede3bf565f6e2226d4495221e2bff7f7
24c02 <163450896+24c02@users.noreply.github.com>
2026-04-01 20:03:33 -0400

    other way is nicer!

--- _plugins/slack_user_tag.rb
+++ _plugins/slack_user_tag.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-# use like so: {% slack_user UDK5M9Y13, cwalker %} => @cwalker, but nicely linked
+# use like so: {% slack_user cwalker, UDK5M9Y13 %} => @cwalker, but nicely linked
 module SlackUserTag
   SLACK_BASE_URL = "https://hackclub.slack.com/team"
 
   class Tag < Liquid::Tag
     def initialize(tag_name, markup, tokens)
       super
-      @id, @name = markup.split(",", 2).map(&:strip)
+      @name, _, @id = markup.rpartition(",").map(&:strip)
     end
 
     def render(_ctx) = %[<a href="#{SLACK_BASE_URL}/#{@id}" class="slack_user" target="_blank">@#{@name}</a>]
